ae175b220784b4c195c4241b0b365d3d6ffb639c
Mike Harley <mharley12345@gmail.com>
2020-03-09 23:33:25 -0700

    .

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ readline        = require('readline'),
 {google}        = require('googleapis'),
 request         = require('request'),
 server             = express();
-server.set("view engine", "ejs");
+
 const port = process.env.PORT || 5000
+
 server.use(express.static('public'))
 server.get('/', (req, res) => {
     res.status(200).send('LIVE.html')
